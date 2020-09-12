2a83c680f89c2c6b6870f27b6f5d0381fdff281f
Daniel C <developer@macoz.co.ke>
2020-09-12 22:52:37 +0300

    Updates

---
+++ precache-manifest.0d7dd3306e9f3d9752666d5146ae5a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca8be9ec0ad140a7a5df5c1bb81ad32",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "dff65b57b5f1631ac823",
+    "url": "/random-quote-machine/static/css/main.715aead8.chunk.css"
+  },
+  {
+    "revision": "f6808a2c4946f5206ca3",
+    "url": "/random-quote-machine/static/js/2.42151cc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.42151cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff65b57b5f1631ac823",
+    "url": "/random-quote-machine/static/js/main.1135e3b6.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
